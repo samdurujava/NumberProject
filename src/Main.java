d491c894d1815906ac1578324b159c183a916a33
GBTC408003UR <GBTC408003UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-20 10:26:03 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+
+        System.out.println(mynumber);
+        System.out.println(myothernumber);
+
+    }
+
+
+}
